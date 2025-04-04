cb4fc03fe74c9a262ce5aa80406d6061b7e37c06
androiddevii <marwa.eltayeb30@gmail.com>
2025-04-04 17:21:02 +0200

    Implement dynamic Sudoku checker functionality

--- src/main/kotlin/sudoku_checker/SudokuChecker.kt
+++ src/main/kotlin/sudoku_checker/SudokuChecker.kt
@@ -4,9 +4,25 @@ fun main() {
 
 }
 
-
 fun isValidSudoku(board: List<List<Char>>, subgridSize: Int = 3): Boolean {
-    return false
+    val gridSize = subgridSize * subgridSize
+    if (board.size != gridSize || board.any { it.size != gridSize }) return false
+
+    val seen = HashSet<String>()
+    board.forEachIndexed { row, line ->
+        line.forEachIndexed { col, value ->
+            if (value != '-') {
+                if (!(value in '1'..'9' || (gridSize > 9 && value in 'A'..'Z' && (value - 'A' + 10) <= gridSize))) return false
+
+                if (!seen.add("$value in row $row") ||
+                    !seen.add("$value in column $col") ||
+                    !seen.add("$value in block ${row / subgridSize}-${col / subgridSize}")) {
+                    return false
+                }
+            }
+        }
+    }
+    return true
 }
 
 
